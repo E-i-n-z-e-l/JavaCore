872c78dfe75c6a8cf797bd2b63a031d8a7120bd0
Einzel <einzel0102@mail.ru>
2024-01-23 16:26:17 +0800

    "Семинар №4 - Практическая №4"

---
+++ Seminar04/src/main/java/onlineShop/Product.java
@@ -0,0 +1,18 @@
+package onlineShop;
+
+import lombok.Data;
+
+import java.math.BigDecimal;
+@Data
+public class Product {
+    private String name;
+    private BigDecimal price;
+    public Product(String name, BigDecimal price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public Object getName() {
+        return name;
+    }
+}
